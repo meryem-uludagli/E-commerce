d79a2fbcf63fbcd69ec278bfc141668c0635dcd1
meryem-uludagli <meryemuslu2035@gmail.com>
2024-11-28 14:57:21 +0200

    first commit

---
+++ src/app/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import slideReducer from "../features/slices/sliderSlice";
+export const store = configureStore({
+  reducer: {
+    slider: slideReducer,
+  },
+});
